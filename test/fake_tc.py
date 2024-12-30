3f4e2042f52e58a0dd6dc4698a11094a7caf4bdd
Casper Andersson <casper.casan@gmail.com>
2024-12-31 00:40:46 +0100

    test: Make fake_tc.py modify correctionField and forward both ways
    Signed-off-by: Casper Andersson <casper.casan@gmail.com>

--- test/fake_tc.py
+++ test/fake_tc.py
@@ -1,24 +1,28 @@
 from scapy.all import *
 
 i = 0
+corrfield = b"\x00\x00\x00\x01\x00\x00"
 
 def change_send(pkt):
     payload = pkt.lastlayer()
-    payload.load = payload.load[:8] + b"\x00\x00\x00\x01\x00\x00" + payload.load[14:]
+    # Sniffing on both and forwarding causes it to sniff its own packets
+    # Skip packets we already modified/forwarded to avoid infinite loops
+    if payload.load[8:14] == corrfield:
+        return
+
+    payload.load = payload.load[:8] + corrfield + payload.load[14:]
     global i
 
-    # if pkt.sniffed_on == 'veth2':
-    sendp(pkt, iface='veth3', verbose=False)
-    print(f"{i}: Forwarded veth2 -> veth3")
+    if pkt.sniffed_on == 'veth2':
+        sendp(pkt, iface='veth3', verbose=False)
+        print(f"{i}: Forwarded veth2 -> veth3")
+    elif pkt.sniffed_on == 'veth3':
+        sendp(pkt, iface='veth2', verbose=False)
+        print(f"{i}: Forwarded veth3 -> veth2")
+
     i += 1
-    # elif pkt.sniffed_on == 'veth3':
-        # sendp(pkt, iface='veth2', verbose=False)
-        # print("Forwarded veth3 -> veth2")
 
-
-# Sniffing on both and forwarding causes it to sniff its own packets
-# sniff(iface=['veth2', 'veth3'], prn=change_send)
-sniff(iface='veth2', prn=change_send)
+sniff(filter='ether proto 0x88f7', iface=['veth2', 'veth3'], prn=change_send)
 
 
 
