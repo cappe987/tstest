397245591d8632571ffa3f20b263877fd47bb110
Casper Andersson <casper.casan@gmail.com>
2026-06-01 08:49:09 +0200

    plot: Improve design

--- scripts/plot.py
+++ scripts/plot.py
@@ -1,14 +1,32 @@
 
 from matplotlib.backends.backend_pdf import PdfPages
 from matplotlib.font_manager import FontProperties
+import matplotlib.ticker as mticker
 import matplotlib.pyplot as plt
 import numpy as np
+import math
 
 def make_page(info, t, s):
     AX_GRAPH = 0
     AX_TABLE = 1
     fig, ax = plt.subplots(2, height_ratios=[2, 1])
+    # Attemps to fix plot width going outside page
+    # fig, ax = plt.subplots(2, height_ratios=[2, 1], tight_layout={"pad": 3, "h_pad": 5})
+    # fig, ax = plt.subplots(2, height_ratios=[2, 1], tight_layout={"rect": (0, 0, 0.9, 0.9)})
     ax[AX_GRAPH].margins(0, 0.2)
+
+    # Avoid formatting y-axis using scientific notation for large numbers
+    ax[AX_GRAPH].ticklabel_format(useOffset=False, style="plain")
+
+    # Add space for thousand separator (1 000 000 for 1000000)
+    # Special handling is need because we don't want decimal point
+    # when it's not needed.
+    def formatter(x, pos):
+        if x.is_integer():
+            return "{:,.0f}".format(x).replace(",", " ")
+        return "{:}".format(x)
+    ax[AX_GRAPH].yaxis.set_major_formatter(formatter)
+
     ax[AX_GRAPH].plot(t, s, color=info['color']) #, label="Time Error")
     # ax[0].legend()
 
@@ -48,8 +66,13 @@ texts = {
     'SYNC_TIME_ERROR': {'title': 'Sync Time Error', 'ylabel': 'Time Error [ns]', 'color': 'blue'},
     'DELAY_TIME_ERROR': {'title': 'Delay Time Error', 'ylabel': 'Time Error [ns]', 'color': 'blue'},
     'TWOWAY_TIME_ERROR': {'title': 'Twoway Time Error', 'ylabel': 'Time Error [ns]', 'color': 'blue'},
+    'MEASURED_PDELAY': {'title': 'Measured Peer Delay', 'ylabel': 'Peer Delay [ns]', 'color': 'blue'},
+    'FWD_ACCURACY': {'title': 'Forward Accuracy', 'ylabel': 'Time Error [ns]', 'color': 'blue'},
+
     'SYNC_LATENCY': {'title': 'Sync Latency', 'ylabel': 'Latency [ns]', 'color': 'green'},
     'DELAY_LATENCY': {'title': 'Delay Latency', 'ylabel': 'Latency [ns]', 'color': 'green'},
+    'PDELAY_LATENCY': {'title': 'Peer Delay Latency', 'ylabel': 'Latency [ns]', 'color': 'green'},
+
     'SYNC_PDV': {'title': 'Sync PDV (Packet Delay Variance)', 'ylabel': 'PDV [ns]', 'color': 'red'},
 }
 
@@ -69,7 +92,7 @@ for x in data:
     time=arr[:, 0]
     values=arr[:, 1]
     fig = make_page(texts[measure], time, values)
-    fig.set_size_inches(8.5, 11)
+    fig.set_size_inches(8.27, 11.69)
     fig.savefig(pdf, format='pdf')
 
 pdf.close()
